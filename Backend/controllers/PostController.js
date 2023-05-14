74e30189201b5a5b9367fde769f9558f171bda39
Пользователь <plat_mv@mail.ru>
2023-05-14 16:37:11 +0300

    Display tags backend fix

--- Backend/controllers/PostController.js
+++ Backend/controllers/PostController.js
@@ -97,7 +97,7 @@ export const create = async (req, res) => {
       title: req.body.title,
       text: req.body.text,
       imageUrl: req.body.imageUrl,
-      tags: req.body.tags,
+      tags: req.body.tags.split(','),
       user: req.userId,
     });
 
@@ -124,7 +124,7 @@ export const update = async (req, res) => {
         title: req.body.title,
         text: req.body.text,
         imageUrl: req.body.imageUrl,
-        tags: req.body.tags,
+        tags: req.body.tags.split(','),
         user: req.userId,
       },
     );
